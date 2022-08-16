ed6e13e412be57e42a808d4001e4d6bb764ed361
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-16 15:55:00 +0900

    Dランク速解きセット:【早解2】正n角形の内角

--- src/paiza/Dランク速解きセット/Main.java
+++ src/paiza/Dランク速解きセット/Main.java
@@ -3,20 +3,30 @@ package paiza.Dランク速解きセット;
 import java.util.*;
 
 
-//【早解1】1日1万歩
+////【早解1】1日1万歩
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//        String[] numbers = input.split(" ");
+//        int d = Integer.parseInt(numbers[0]);
+//        int s = Integer.parseInt(numbers[1]);
+//
+//        int result = (d * 100000) / s;
+//        if (result >= 10000) {
+//            System.out.println("yes");
+//        } else {
+//            System.out.println("no");
+//        }
+//    }
+//}
+
+//【早解2】正n角形の内角
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] numbers = input.split(" ");
-        int d = Integer.parseInt(numbers[0]);
-        int s = Integer.parseInt(numbers[1]);
-
-        int result = (d * 100000) / s;
-        if (result >= 10000) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
+        int num = Integer.parseInt(sc.nextLine());
+        int result = (180*(num - 2))/num;
+        System.out.println(result);
     }
 }
