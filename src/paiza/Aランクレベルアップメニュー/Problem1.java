86c8c94e991faf884b120fac1ba11449ba266da6
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-18 16:04:20 +0900

    Aランクレベルアップメニュー マップの判定・縦横 STEP: 1 盤面の情報取得

--- src/paiza/Aランクレベルアップメニュー/Problem1.java
+++ src/paiza/Aランクレベルアップメニュー/Problem1.java
@@ -7,37 +7,20 @@ public class Problem1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] initializers = sc.nextLine().split(" ");
-        int h = Integer.parseInt(initializers[0]);
-        int w = Integer.parseInt(initializers[1]);
-        int n = Integer.parseInt(initializers[2]);
+        int height = Integer.parseInt(initializers[0]);
+        int width = Integer.parseInt(initializers[1]);
+        int totalGivenCoord =Integer.parseInt(initializers[2]);
 
-        Map<Integer, String> rows = new HashMap<>();
-        for (int i = 0; i < h; i++) {
-            String input = sc.nextLine();
-            rows.put(i, input);
+        String[] map = new String[height];
+        for (int row = 0; row < height; row++) {
+            map[row] = sc.nextLine();
         }
 
-        Map<Integer, String> searchCoord = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            String[] input = sc.nextLine().split(" ");
-            String coordX = input[0];
-            String coordY = input[1];
-            searchCoord.put(i, coordX + coordY);
-        }
-
-//        System.out.println(rows);
-//        System.out.println(searchCoord);
-
-        for (int i = 0; i < n; i++) {
-            String coord = searchCoord.get(i);
-            int coordX = Integer.parseInt(String.valueOf(coord.charAt(0)));
-            int coordY = Integer.parseInt(String.valueOf(coord.charAt(1)));
-
-//            System.out.println("XY ->" + coord);
-//            System.out.println("X ->" + coordX);
-//            System.out.println("Y ->" + coordY);
-
-            System.out.println(rows.get(coordX).charAt(coordY));
+        for (int i = 1; i <= totalGivenCoord; i++) {
+            String[] givenCoord = sc.nextLine().split(" ");
+            int givenYCoord = Integer.parseInt(givenCoord[0]);
+            int givenXCoord = Integer.parseInt(givenCoord[1]);
+            System.out.println(map[givenYCoord].charAt(givenXCoord));
         }
     }
 }
