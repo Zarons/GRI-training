e3ebf812f70058232d7891a34bfb3db13a627b6c
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-18 16:40:39 +0900

    Aランクレベルアップメニュー マップの判定・縦横 STEP: 4 マップの判定・横

--- src/paiza/Aランクレベルアップメニュー/Problem4.java
+++ src/paiza/Aランクレベルアップメニュー/Problem4.java
@@ -4,41 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-//マップの判定・縦
+//STEP: 4 マップの判定・縦
 public class Problem4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] initializers = sc.nextLine().split(" ");
-        int h = Integer.parseInt(initializers[0]);
-        int w = Integer.parseInt(initializers[1]);
+        int height = Integer.parseInt(initializers[0]);
+        int width = Integer.parseInt(initializers[1]);
 
-        Map<Integer, String> rows = new HashMap<>();
-        for (int i = 0; i < h; i++) {
+        char[][] coordArray = new char[height][width];
+        for (int row = 0; row < height; row++) {
             String input = sc.nextLine();
-            rows.put(i, input);
+            for (int col = 0; col < width; col++) {
+                coordArray[row][col] = input.charAt(col);
+            }
         }
 
-        for (int i = 0; i < h; i++) {
-            if (i == 0) {
-                char[] botArray = rows.get(1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (botArray[j] == '#') {
-                        System.out.println(0 + " " + j);
-                    }
-                }
-            } else if (i == h - 1) {
-                char[] topArray = rows.get(i - 1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (topArray[j] == '#') {
-                        System.out.println(i + " " + j);
-                    }
-                }
-            } else if (i < h - 1) {
-                char[] topArray = rows.get(i - 1).toCharArray();
-                char[] botArray = rows.get(i + 1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (topArray[j] == '#' && botArray[j] == '#') {
-                        System.out.println(i + " " + j);
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (row == 0 || coordArray[row - 1][col] == '#') {
+                    if (row == height - 1 || coordArray[row + 1][col] == '#') {
+                        System.out.println(row + " " + col);
                     }
                 }
             }
