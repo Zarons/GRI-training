cae690c926bdfdbd3c3f15b30814693bd6215497
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-19 12:31:03 +0900

    Aランクレベルアップメニュー マップの判定・縦横 STEP: 3 マップの判定・横

--- src/paiza/Aランクレベルアップメニュー/Problem3.java
+++ src/paiza/Aランクレベルアップメニュー/Problem3.java
@@ -8,16 +8,12 @@ import java.util.Scanner;
 public class Problem3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] initializers = sc.nextLine().split(" ");
-        int height = Integer.parseInt(initializers[0]);
-        int width = Integer.parseInt(initializers[1]);
+        int height = sc.nextInt();
+        int width = sc.nextInt(); sc.nextLine();
 
         char[][] coordArray = new char[height][width];
         for (int row = 0; row < height; row++) {
-            String input = sc.nextLine();
-            for (int col = 0; col < width; col++) {
-                coordArray[row][col] = input.charAt(col);
-            }
+            coordArray[row] = sc.nextLine().toCharArray();
         }
 
         for (int row = 0; row < height; row++) {
