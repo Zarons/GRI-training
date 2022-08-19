be558f4d6d5f929e660f4947ea9a9eff694bd1e5
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-19 15:04:02 +0900

    Aランクレベルアップメニュー 座標系での向きの変わる移動 STEP: 4 座標系での規則的な移動

--- src/paiza/Aランクレベルアップメニュー/Problem9.java
+++ src/paiza/Aランクレベルアップメニュー/Problem9.java
@@ -1,46 +1,36 @@
 package paiza.Aランクレベルアップメニュー;
 
-import java.util.Scanner;
+import java.util.*;
 
 //座標系での規則的な移動
 public class Problem9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] initializers = sc.nextLine().split(" ");
-        int xCoord = Integer.parseInt(initializers[0]);
-        int yCoord = Integer.parseInt(initializers[1]);
-        int maxMoveCount = Integer.parseInt(initializers[2]);
+        int xCoord = sc.nextInt();
+        int yCoord = sc.nextInt();
+        int maxMoveCount = sc.nextInt();
 
         char[] direction = {'E', 'S', 'W', 'N'};
-        final int maxDirIndex = direction.length;
-        int curDirIndex = 0;
-
+        int currentDirection = 0;
         int currentMove = 0;
         int maxMoveBeforeTurning = 1;
-        int countTurning = 0;
-
+        int turned = 0;
         for (int i = 0; i < maxMoveCount; i++) {
-            currentMove++;
-            switch (direction[curDirIndex]) {
+            switch (direction[currentDirection % 4]) {
                 case 'E' -> xCoord += 1;
                 case 'S' -> yCoord += 1;
                 case 'W' -> xCoord -= 1;
                 case 'N' -> yCoord -= 1;
             }
-
+            currentMove++;
             if (currentMove >= maxMoveBeforeTurning) {
-                curDirIndex++;
-                if (curDirIndex >= maxDirIndex) {
-                    curDirIndex = 0;
-                }
-
                 currentMove = 0;
-                countTurning++;
+                currentDirection++;
+                turned++;
             }
-
-            if (countTurning >= 2) {
+            if (turned >= 2) {
                 maxMoveBeforeTurning++;
-                countTurning = 0;
+                turned = 0;
             }
         }
         System.out.println(xCoord + " " + yCoord);
