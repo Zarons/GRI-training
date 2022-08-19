443cbcd7beb6d615ad148c958df8b563a51f2f76
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-19 14:22:24 +0900

    Aランクレベルアップメニュー 座標系での向きの変わる移動 STEP: 2 座標系での移動・方角

--- src/paiza/Aランクレベルアップメニュー/Problem7.java
+++ src/paiza/Aランクレベルアップメニュー/Problem7.java
@@ -8,22 +8,18 @@ import java.util.Scanner;
 public class Problem7 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] initializers = sc.nextLine().split(" ");
-        int yCoord = Integer.parseInt(initializers[0]);
-        int xCoord = Integer.parseInt(initializers[1]);
-        int moveCount = Integer.parseInt(initializers[2]);
+        int yCoord = sc.nextInt();
+        int xCoord = sc.nextInt();
+        int totalMoves = sc.nextInt();
+        sc.nextLine();
 
-        String moveSequence = "";
-        for (int i = 0; i < moveCount; i++) {
-            moveSequence += sc.nextLine();
-        }
-
-        for (int i = 0; i < moveCount; i++) {
-            switch (moveSequence.charAt(i)) {
-                case 'N' -> yCoord -= 1;
-                case 'S' -> yCoord += 1;
-                case 'W' -> xCoord -= 1;
-                case 'E' -> xCoord += 1;
+        for (int i = 0; i < totalMoves; i++) {
+            String moveInput = sc.nextLine();
+            switch (moveInput) {
+                case "N" -> yCoord -= 1;
+                case "S" -> yCoord += 1;
+                case "W" -> xCoord -= 1;
+                case "E" -> xCoord += 1;
             }
             System.out.println(yCoord + " " + xCoord);
         }
