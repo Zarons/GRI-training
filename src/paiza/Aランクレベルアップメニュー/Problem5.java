4183a40756b8e16d8680980fd92ecc4b918ba926
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-19 13:40:26 +0900

    Aランクレベルアップメニュー マップの判定・縦横 STEP: 5 マップの判定・縦横

--- src/paiza/Aランクレベルアップメニュー/Problem5.java
+++ src/paiza/Aランクレベルアップメニュー/Problem5.java
@@ -1,79 +1,27 @@
 package paiza.Aランクレベルアップメニュー;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 //マップの判定・縦横
 public class Problem5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] initializers = sc.nextLine().split(" ");
-        int h = Integer.parseInt(initializers[0]);
-        int w = Integer.parseInt(initializers[1]);
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+        sc.nextLine();
 
-        Map<Integer, String> rows = new HashMap<>();
-        for (int i = 0; i < h; i++) {
-            String input = sc.nextLine();
-            rows.put(i, input);
+        char[][] coordArray = new char[height][width];
+        for (int row = 0; row < height; row++) {
+            coordArray[row] = sc.nextLine().toCharArray();
         }
 
-        for (int i = 0; i < h; i++) {
-            if (i == 0) {
-                char[] topArray = rows.get(i).toCharArray();
-                char[] botArray = rows.get(i + 1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (j == 0) {
-                        if (topArray[j + 1] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else if (j == w - 1) {
-                        if (topArray[j - 1] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else {
-                        if (topArray[j - 1] == '#' && topArray[j + 1] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    }
-                }
-            } else if (i == h - 1) {
-                char[] botArray = rows.get(i).toCharArray();
-                char[] topArray = rows.get(i - 1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (j == 0) {
-                        if (botArray[j + 1] == '#' && topArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else if (j == w - 1) {
-                        if (botArray[j - 1] == '#' && topArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else {
-                        if (botArray[j - 1] == '#' && botArray[j + 1] == '#' && topArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    }
-                }
-            } else {
-                char[] topArray = rows.get(i - 1).toCharArray();
-                char[] midArray = rows.get(i).toCharArray();
-                char[] botArray = rows.get(i + 1).toCharArray();
-                for (int j = 0; j < w; j++) {
-                    if (j == 0) {
-                        if (midArray[j + 1] == '#' && topArray[j] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else if (j == w - 1) {
-                        if (midArray[j - 1] == '#' && topArray[j] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    } else {
-                        if (midArray[j - 1] == '#' && midArray[j + 1] == '#' && topArray[j] == '#' && botArray[j] == '#') {
-                            System.out.println(i + " " + j);
-                        }
-                    }
-                }
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                if (col > 0 && coordArray[row][col - 1] == '.') continue;
+                if (col < width - 1 && coordArray[row][col + 1] == '.') continue;
+                if (row > 0 && coordArray[row - 1][col] == '.') continue;
+                if (row < height - 1 && coordArray[row + 1][col] == '.') continue;
+                System.out.println(row + " " + col);
             }
         }
     }
