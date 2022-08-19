712f515662c38cb585da6733664d177e2054c3ee
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-19 17:33:01 +0900

    Aランクレベルアップメニュー へび STEP: 1 移動が可能かの判定・方角

--- src/paiza/Aランクレベルアップメニュー/Problem11.java
+++ src/paiza/Aランクレベルアップメニュー/Problem11.java
@@ -1,57 +1,34 @@
 package paiza.Aランクレベルアップメニュー;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 //移動が可能かの判定・方角
 public class Problem11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] initializers = sc.nextLine().split(" ");
-        int height = Integer.parseInt(initializers[0]);
-        int width = Integer.parseInt(initializers[1]);
-        int yCoord = Integer.parseInt(initializers[2]);
-        int xCoord = Integer.parseInt(initializers[3]);
-        String direction = initializers[4];
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+        int yCoord = sc.nextInt();
+        int xCoord = sc.nextInt();
+        String direction = sc.next();
+        sc.nextLine();
 
-        Map<Integer, String> rows = new HashMap<>();
-        for (int i = 0; i < height; i++) {
-            String input = sc.nextLine().substring(0, width);
-            rows.put(i, input);
+        char[][] coordArray = new char[height][width];
+        for (int row = 0; row < height; row++) {
+            coordArray[row] = sc.nextLine().toCharArray();
         }
 
-        if (direction.equals("N") && yCoord == 0 || yCoord >= height || xCoord >= width ) {
-            System.out.println("No");
-        } else if (direction.equals("S") && yCoord == height - 1) {
-            System.out.println("No");
-        } else if (direction.equals("E") && yCoord == width - 1) {
-            System.out.println("No");
-        } else if (direction.equals("W") && xCoord == 0) {
+        switch (direction) {
+            case "N" -> yCoord -= 1;
+            case "E" -> xCoord += 1;
+            case "S" -> yCoord += 1;
+            case "W" -> xCoord -= 1;
+        }
+
+        if (xCoord < 0 || xCoord >= width || yCoord < 0 || yCoord >= height || coordArray[yCoord][xCoord] == '#') {
             System.out.println("No");
         } else {
-            switch (direction) {
-                case "N" -> {
-                    char[] row = rows.get(yCoord - 1).toCharArray();
-                    if (row[xCoord] == '#') System.out.println("No");
-                    else System.out.println("Yes");
-                }
-                case "S" -> {
-                    char[] row = rows.get(yCoord + 1).toCharArray();
-                    if (row[xCoord] == '#') System.out.println("No");
-                    else System.out.println("Yes");
-                }
-                case "E" -> {
-                    char[] row = rows.get(yCoord).toCharArray();
-                    if (row[yCoord + 1] == '#') System.out.println("No");
-                    else System.out.println("Yes");
-                }
-                case "W" -> {
-                    char[] row = rows.get(yCoord).toCharArray();
-                    if (row[yCoord - 1] == '#') System.out.println("No");
-                    else System.out.println("Yes");
-                }
-            }
+            System.out.println("Yes");
         }
     }
 }
