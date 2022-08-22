cb5ee17d2b50fa02ec614fbdd0bba86f96baf91b
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-22 14:16:46 +0900

    Aランクレベルアップメニュー 陣取りゲーム STEP: 1 マスの陣取り

---
+++ src/paiza/Aランクレベルアップメニュー/Problem4_1.java
@@ -0,0 +1,34 @@
+package paiza.Aランクレベルアップメニュー;
+
+import java.util.Scanner;
+
+//マスの陣取り
+public class Problem4_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sy = sc.nextInt();
+        int sx = sc.nextInt();
+        sc.nextLine();
+
+        char[][] map = new char[sy][sx];
+        for (int row = 0; row < sy; row++) {
+            map[row] = sc.nextLine().toCharArray();
+        }
+
+        for (int row = 0; row < sy; row++) {
+            for (int col = 0; col < sx; col++) {
+                if (col > 0 && map[row][col - 1] == '*') map[row][col] = 'M';
+                if (col < sx - 1 && map[row][col + 1] == '*') map[row][col] = 'M';
+                if (row > 0 && map[row - 1][col] == '*') map[row][col] = 'M';
+                if (row < sy - 1 && map[row + 1][col] == '*') map[row][col] = 'M';
+            }
+        }
+
+        for (int row = 0; row < sy; row++) {
+            for (int col = 0; col < sx; col++) {
+                if (map[row][col] == 'M') map[row][col] = '*';
+            }
+            System.out.println(map[row]);
+        }
+    }
+}
