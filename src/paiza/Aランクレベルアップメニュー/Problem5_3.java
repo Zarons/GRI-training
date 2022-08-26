5a5153f9f3069364688c99ebe3a3153c265cfcce
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-26 17:43:47 +0900

    Aランクレベルアップメニュー いびつなリバーシ対戦 STEP: 3 裏返せる可能性（斜め）

---
+++ src/paiza/Aランクレベルアップメニュー/Problem5_3.java
@@ -0,0 +1,29 @@
+package paiza.Aランクレベルアップメニュー;
+
+import java.util.Scanner;
+
+// STEP: 3 裏返せる可能性（斜め）
+public class Problem5_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int height = sc.nextInt();
+        int width = sc.nextInt();
+        int sy = sc.nextInt();
+        int sx = sc.nextInt();
+
+        //二次元配列の代入
+        char[][] map = new char[height][width];
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                //斜め数学パターン
+                if ((row - sy) - (col - sx) == 0 || (row - sy) + (col - sx) == 0 ) map[row][col] = '*';
+                else map[row][col] = '.';
+            }
+        }
+        map[sy][sx] = '!';
+        //print new map
+        for (int row = 0; row < height; row++) {
+            System.out.println(map[row]);
+        }
+    }
+}
