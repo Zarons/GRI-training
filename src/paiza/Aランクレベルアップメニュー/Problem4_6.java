8dcdde3e451ac936f25d7223904d6e8424e99754
Geryenko Hawsen <ger.zarons@gmail.com>
2022-08-26 17:43:37 +0900

    Aランクレベルアップメニュー 陣取りゲーム FINAL 陣取りゲーム

--- src/paiza/Aランクレベルアップメニュー/Problem4_6.java
+++ src/paiza/Aランクレベルアップメニュー/Problem4_6.java
@@ -1,6 +1,8 @@
 package paiza.Aランクレベルアップメニュー;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.awt.Point;
 
 //FINAL 陣取りゲーム
 public class Problem4_6 {
@@ -9,11 +11,8 @@ public class Problem4_6 {
         int sy = sc.nextInt();
         int sx = sc.nextInt();
         sc.nextLine();
-
-        char firstChar = sc.next().charAt(0); // スタート char を決める（A / B）
-        char secondChar = firstChar == 'A' ? 'B' : 'A';
-        char firstTempChar = firstChar == 'A' ? 'a' : 'b'; // 操作を交互に繰り返すため
-        char secondTempChar = firstChar == 'A' ? 'b' : 'a';
+        char char1 = sc.next().charAt(0); // スタート char を決める（A / B）
+        char char2 = char1 == 'A' ? 'B' : 'A';
         sc.nextLine();
 
         char[][] map = new char[sy][sx]; // 二次元配列
@@ -21,109 +20,59 @@ public class Problem4_6 {
             map[row] = sc.nextLine().toCharArray();
         }
 
-        int checkedFirstChar = 0;
-        int countAddedFirstChar = 0;
-        int newlyAddedFirstChar = 1; //最初の firstChar は必ず1個ある
-        int checkedSecondChar = 0;
-        int countAddedSecondChar = 0;
-        int newlyAddedSecondChar = 1; //最初の secondChar は必ず1個ある
-        for (int i = 0; i < sy * sx; i++) { // 全部の座標を確認する
-            firstCharLoop:
-            for (int row = 0; row < sy; row++) {  // firstCharを見つけるまでループする
-                if (String.valueOf(map[row]).indexOf(firstChar) != -1) { // もっと早い？
-                    for (int col = 0; col < sx; col++) { // 1列を確認する
-                        if (map[row][col] == firstChar) {
-                            map[row][col] = 'X';
-                            checkedFirstChar += 1;
-                            if (col + 1 < sx && map[row][col + 1] != '#' && map[row][col + 1] == '.') { // 右の表座を確認する
-                                map[row][col + 1] = firstTempChar;
-                                countAddedFirstChar += 1;
-                            }
-                            if (col - 1 >= 0 && map[row][col - 1] != '#' && map[row][col - 1] == '.') { // 左の表座を確認する
-                                map[row][col - 1] = firstTempChar;
-                                countAddedFirstChar += 1;
-                            }
-                            if (row - 1 >= 0 && map[row - 1][col] != '#' && map[row - 1][col] == '.') { // 上の表座を確認する
-                                map[row - 1][col] = firstTempChar;
-                                countAddedFirstChar += 1;
-                            }
-                            if (row + 1 < sy && map[row + 1][col] != '#' && map[row + 1][col] == '.') { // 下の表座を確認する
-                                map[row + 1][col] = firstTempChar;
-                                countAddedFirstChar += 1;
-                            }
-                            if (checkedFirstChar == newlyAddedFirstChar) {
-                                newlyAddedFirstChar = countAddedFirstChar;
-                                checkedFirstChar = 0;
-                                countAddedFirstChar = 0;
-                                break firstCharLoop;
-                            }
-                        }
-                    }
-                }
-            }
-            secondCharLoop:
-            for (int row = 0; row < sy; row++) { // firstCharを見つけたら、secondCharを探す
-                if (String.valueOf(map[row]).indexOf(secondChar) != -1) {
-                    for (int col = 0; col < sx; col++) {
-                        if (map[row][col] == secondChar) {
-                            map[row][col] = 'Y';
-                            checkedSecondChar += 1;
-                            if (col + 1 < sx && map[row][col + 1] != '#' && map[row][col + 1] == '.') { // 右の表座を確認する
-                                map[row][col + 1] = secondTempChar;
-                                countAddedSecondChar += 1;
-                            }
-                            if (col - 1 >= 0 && map[row][col - 1] != '#' && map[row][col - 1] == '.') { // 左の表座を確認する
-                                map[row][col - 1] = secondTempChar;
-                                countAddedSecondChar += 1;
-                            }
-                            if (row - 1 >= 0 && map[row - 1][col] != '#' && map[row - 1][col] == '.') { // 上の表座を確認する
-                                map[row - 1][col] = secondTempChar;
-                                countAddedSecondChar += 1;
-                            }
-                            if (row + 1 < sy && map[row + 1][col] != '#' && map[row + 1][col] == '.') { // 下の表座を確認する
-                                map[row + 1][col] = secondTempChar;
-                                countAddedSecondChar += 1;
-                            }
-                            if (checkedSecondChar == newlyAddedSecondChar) {
-                                newlyAddedSecondChar = countAddedSecondChar;
-                                checkedSecondChar = 0;
-                                countAddedSecondChar = 0;
-                                break secondCharLoop;
-                            }
-                        }
-                    }
-                }
-            }
-            boolean noMoreValidMoves = true;
-            for (int row = 0; row < sy; row++) { // firstCharとsecondCharの上下左右を確認したら、TempCharを変更する
-                if (String.valueOf(map[row]).indexOf('a') != -1 || String.valueOf(map[row]).indexOf('b') != -1) {
-                    noMoreValidMoves = false;
-                    for (int col = 0; col < sx; col++) {
-                        if (map[row][col] == 'a') map[row][col] = 'A';
-                        if (map[row][col] == 'b') map[row][col] = 'B';
-                    }
+        ArrayList<Point> checkList1 = new ArrayList<>(); // 先攻用
+        findStartingCoord(sy, sx, char1, map, checkList1);
+        ArrayList<Point> newCoordList1 = new ArrayList<>(); // 後攻用
+        boolean checkList1IsEmpty = false;
+
+        ArrayList<Point> checkList2 = new ArrayList<>(); // 先攻用
+        findStartingCoord(sy, sx, char2, map, checkList2);
+        ArrayList<Point> newCoordList2 = new ArrayList<>(); // 後攻用
+        boolean checkList2IsEmpty = false;
+
+        while (!checkList1IsEmpty || !checkList2IsEmpty) {
+            int i = 0;
+            while (i < checkList1.size()) {
+                Point currentCoord = checkList1.get(i);
+                int row = (int) currentCoord.getY();
+                int col = (int) currentCoord.getX();
+                checkNorth(row, col, 0, char1, map, newCoordList1);
+                checkEast(row, col, sx, char1, map, newCoordList1);
+                checkSouth(row, col, sy, char1, map, newCoordList1);
+                checkWest(row, col, 0, char1, map, newCoordList1);
+                if (i == checkList1.size() - 1 && newCoordList1.isEmpty()) { // empty list1
+                    checkList1.subList(0, checkList1.size()).clear();
+                    checkList1IsEmpty = true;
+                    break;
+                } else if (i < checkList1.size() - 1) {
+                    i++;
+                } else if (i == checkList1.size() - 1 && !newCoordList1.isEmpty()) {
+                    renewCheckList(checkList1, newCoordList1);
+                    break;
                 }
             }
 
-//            System.out.println("==============");
-//            for (int row = 0; row < sy; row++) {
-//                System.out.println(map[row]);
-//            }
-
-            if (noMoreValidMoves) break;
-        }
-
-        for (int row = 0; row < sy; row++) { // firstCharとsecondCharの上下左右を確認したら、TempCharを変更する
-            for (int col = 0; col < sx; col++) {
-                if (map[row][col] == 'X' || map[row][col] == firstTempChar ) map[row][col] = firstChar;
-                if (map[row][col] == 'Y' || map[row][col] == secondTempChar ) map[row][col] = secondChar;
+            int j = 0;
+            while (j < checkList2.size()) {
+                Point currentCoord = checkList2.get(j);
+                int row = (int) currentCoord.getY();
+                int col = (int) currentCoord.getX();
+                checkNorth(row, col, 0, char2, map, newCoordList2);
+                checkEast(row, col, sx, char2, map, newCoordList2);
+                checkSouth(row, col, sy, char2, map, newCoordList2);
+                checkWest(row, col, 0, char2, map, newCoordList2);
+                if (j == checkList2.size() - 1 && newCoordList2.isEmpty()) { // empty list1
+                    checkList2.subList(0, checkList2.size()).clear();
+                    checkList2IsEmpty = true;
+                    break;
+                } else if (j < checkList2.size() - 1) {
+                    j++;
+                } else if (j == checkList2.size() - 1 && !newCoordList2.isEmpty()) {
+                    renewCheckList(checkList2, newCoordList2);
+                    break;
+                }
             }
         }
-
-//        System.out.println("==============");
-//        for (int row = 0; row < sy; row++) {
-//            System.out.println(map[row]);
-//        }
 
         int countA = 0;
         int countB = 0;
@@ -136,5 +85,56 @@ public class Problem4_6 {
         String winner = countA > countB ? "A" : "B"; // 勝者を決める
         System.out.println(countA + " " + countB);
         System.out.println(winner);
+    }
+
+    public static void findStartingCoord(int sy, int sx, char inputChar, char[][] map, ArrayList<Point> list) {
+        for (int row = 0; row < sy; row++) {
+            if (String.valueOf(map[row]).indexOf(inputChar) != -1) {
+                for (int col = 0; col < sx; col++) {
+                    if (map[row][col] == inputChar) {
+                        Point point = new Point(col, row);
+                        list.add(point);
+                    }
+                }
+            }
+        }
+    }
+
+    public static void checkNorth(int row, int col, int northLimit, char inputChar, char[][] map, ArrayList<Point> newList) {
+        if (row - 1 >= northLimit && map[row - 1][col] != '#' && map[row - 1][col] == '.') { // 上の表座を確認する
+            map[row - 1][col] = inputChar;
+            Point point = new Point(col, row - 1);
+            newList.add(point);
+        }
+    }
+
+    public static void checkSouth(int row, int col, int southLimit, char inputChar, char[][] map, ArrayList<Point> newList) {
+        if (row + 1 < southLimit && map[row + 1][col] != '#' && map[row + 1][col] == '.') { // 下の表座を確認する
+            map[row + 1][col] = inputChar;
+            Point point = new Point(col, row + 1);
+            newList.add(point);
+        }
+    }
+
+    public static void checkEast(int row, int col, int eastLimit, char inputChar, char[][] map, ArrayList<Point> newList) {
+        if (col + 1 < eastLimit && map[row][col + 1] != '#' && map[row][col + 1] == '.') { // 右の表座を確認する
+            map[row][col + 1] = inputChar;
+            Point point = new Point(col + 1, row);
+            newList.add(point);
+        }
+    }
+
+    public static void checkWest(int row, int col, int westLimit, char inputChar, char[][] map, ArrayList<Point> newList) {
+        if (col - 1 >= westLimit && map[row][col - 1] != '#' && map[row][col - 1] == '.') { // 左の表座を確認する
+            map[row][col - 1] = inputChar;
+            Point point = new Point(col - 1, row);
+            newList.add(point);
+        }
+    }
+
+    public static void renewCheckList (ArrayList<Point> checkList, ArrayList<Point> newList) {
+        checkList.subList(0, checkList.size()).clear();
+        checkList.addAll(newList);
+        newList.subList(0, newList.size()).clear();
     }
 }
