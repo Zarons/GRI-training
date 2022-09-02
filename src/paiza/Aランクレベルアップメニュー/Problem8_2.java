60bd59c6189ed14ba2e74e0ddae8f46e36dd6e0b
Geryenko Hawsen <ger.zarons@gmail.com>
2022-09-02 17:22:41 +0900

    Aランクレベルアップメニュー 連結の判定 STEP: 2 隣接リスト

---
+++ src/paiza/Aランクレベルアップメニュー/Problem8_2.java
@@ -0,0 +1,40 @@
+package paiza.Aランクレベルアップメニュー;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// STEP: 2 隣接リスト
+public class Problem8_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mapSize = sc.nextInt();
+        int totalInput = sc.nextInt();
+
+        char[][] map = new char[mapSize][mapSize];
+        for (int row = 0; row < mapSize; row++) {
+            Arrays.fill(map[row], '0');
+        }
+
+        for (int input = 0; input < totalInput; input++) {
+            int i = sc.nextInt();
+            int j = sc.nextInt();
+            addEdge(i, j, map);
+        }
+
+        for (int row = 0; row < mapSize; row++) {
+            if (new String(map[row]).contains("1")){
+                for (int col = 0; col < mapSize; col++) {
+                    if (map[row][col] == '1') {
+                        System.out.print(col);
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void addEdge(int i, int j, char[][] map) {
+        map[i - 1][j - 1] = '1';
+        map[j - 1][i - 1] = '1';
+    }
+}
