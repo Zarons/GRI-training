4762dd5935be6f7f79ac80315f5459f402997b04
Geryenko Hawsen <ger.zarons@gmail.com>
2022-09-05 17:07:32 +0900

    Aランクレベルアップメニュー 連結の判定 FINAL 連結の判定

---
+++ src/paiza/Aランクレベルアップメニュー/Problem8_7.java
@@ -0,0 +1,32 @@
+package paiza.Aランクレベルアップメニュー;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+// FINAL 連結の判定
+public class Problem8_7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int mapSize = sc.nextInt();
+        int edges = sc.nextInt();
+
+        int[][] map = new int[mapSize][mapSize];
+        for (int edge = 0; edge < edges; edge++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            map[a - 1][b - 1] = 1;
+            map[b - 1][a - 1] = 1;
+        }
+
+        boolean isValid = true;
+        for (int row = 0; row < mapSize; row++) {
+            String checkStr = Arrays.toString(map[row]);
+            if (!checkStr.contains("1")) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid) System.out.println("YES");
+        else System.out.println("NO");
+    }
+}
